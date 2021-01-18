f3dc15b5810600f8bbc178b5afab79bd875fe465
xewoks <76702828+xewoks@users.noreply.github.com>
2021-01-18 14:02:18 -0500

    Add files via upload

---
+++ sql/record_by_id.sql
@@ -0,0 +1,9 @@
+SELECT progress, CASE WHEN players.link_banned THEN NULL ELSE records.video::text END, status_::text AS "status!: String" ,
+       players.id AS player_id, players.name AS "player_name: String", players.banned AS player_banned,
+       demons.id AS demon_id, demons.name AS "demon_name: String", demons.position,
+       submitters.submitter_id AS submitter_id, submitters.banned AS submitter_banned
+FROM records
+INNER JOIN players ON records.player = players.id
+INNER JOIN demons ON records.demon = demons.id
+INNER JOIN submitters ON records.submitter = submitters.submitter_id
+WHERE records.id = $1
